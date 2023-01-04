e344065d13959da5ce5d0fd4fb406da158444ffd
Tatiana <Bowmantatiana2@gmail.com>
2023-01-04 10:45:59 -0800

    added price

--- src/index.js
+++ src/index.js
@@ -36,12 +36,21 @@
     const coffeeName = document.querySelector("h2.name")
     const description = document.querySelector("h3.description")
     const ingredients = document.querySelector("div#coffee-ingredients")
+    const price = document.querySelector("h4#coffee-cost");
 
     function coffeeDescription(coffee) {
       descriptionImg.src = coffee.image;
       coffeeName.textContent = coffee.title;
       description.textContent = coffee.description;
 
+      if(coffee.ingredients.length === 1) {
+        price.textContent = "$1.25";
+      } else if(coffee.ingredients.length === 2) {
+        price.textContent = "$2.00"
+      } else if(coffee.ingredients.length >= 3) {
+        price.textContent = "$3.50"
+      }
+      
       const ingredientList = document.createElement("ul")
       ingredients.append(ingredientList);
 
